da448db89c072c469e9031c41cb8b862495f8f59
Asadal <AsadalTheMighty@protonmail.com>
2021-11-30 16:26:45 -0800

    built structure with page object; added sample script to ensure webdriver functions

---
+++ sample_script.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+# init driver
+driver = webdriver.Chrome(executable_path='/Users/jordan/Projects/chromedriver')
+driver.maximize_window()
+driver.wait = WebDriverWait(driver, 10)
+driver.implicitly_wait(4)
+
+# open the url
+driver.get('https://www.google.com/')
+
+search = driver.find_element(By.NAME, 'q')
+search.clear()
+search.send_keys('Dress')
+
+# wait for 4 sec
+driver.wait.until(EC.element_to_be_clickable((By.NAME, 'btnK')), message='Error, search button is not clickable')
+
+# click search
+driver.find_element(By.NAME, 'btnK').click()
+
+# verify
+assert 'dress' in driver.current_url.lower(), f"Expected query not in {driver.current_url.lower()}"
+print('Test Passed')
+
+driver.quit()
